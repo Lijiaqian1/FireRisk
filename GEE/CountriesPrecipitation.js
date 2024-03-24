f31396c21ca7210f41e10a7ef2e87c8eac544c51
Lijiaqian1 <95671887+Lijiaqian1@users.noreply.github.com>
2024-03-24 23:24:25 +0800

    Update CountriesPrecipitation.js
    Change map color

--- GEE/CountriesPrecipitation.js
+++ GEE/CountriesPrecipitation.js
@@ -1,20 +1,20 @@
 // 导入 TRMM 3B42 数据集并选择日期范围
 var dataset = ee.ImageCollection('TRMM/3B42')
-                  .filter(ee.Filter.date('2018-07-07', '2018-07-10')); // 替换日期为您感兴趣的范围
+                  .filter(ee.Filter.date('2019-07-01', '2019-07-02')); // 替换日期为您感兴趣的范围
 
 // 选择降水数据波段
 var precipitation = dataset.select(['HQprecipitation']).first();
 
 // 设置降水可视化参数
 var precipitationVis = {
-  min: 0,
-  max: 12,
+  min: 0.1,
+  max: 1.2,
   palette: ['blue', 'purple', 'cyan', 'green', 'yellow', 'red'],
 };
 var countries = ee.FeatureCollection('FAO/GAUL/2015/level0'); 
 var italy = countries.filter(ee.Filter.eq('ADM0_NAME', 'Thailand')); // 选择意大利
 
-var geometry = ee.Geometry.Rectangle([92, -10, 140, 23]); // 获取意大利的几何信息
+var geometry = ee.Geometry.Rectangle([92, 9, 110, 23]); // 获取意大利的几何信息
 
 // 将地图中心设置为感兴趣区域
 Map.setCenter(-79.98, 23.32, 4);
@@ -27,7 +27,7 @@ Export.image.toDrive({
   description: 'precipitation_map',
   folder: 'GEE_exports',
   region: geometry,
-  scale: 1000,
+  scale: 10000,
   crs: 'EPSG:4326',
   maxPixels: 1e13
 });
