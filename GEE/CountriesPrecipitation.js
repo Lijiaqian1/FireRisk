c90d4c458c3b57a0193348b5f50a0dff4563e797
Lijiaqian1 <lijiaqian65@gmail.com>
2024-03-24 17:43:58 +0800

    Create CountriesPrecipitation.js

---
+++ GEE/CountriesPrecipitation.js
@@ -0,0 +1,34 @@
+// 导入 TRMM 3B42 数据集并选择日期范围
+var dataset = ee.ImageCollection('TRMM/3B42')
+                  .filter(ee.Filter.date('2018-07-07', '2018-07-10')); // 替换日期为您感兴趣的范围
+
+// 选择降水数据波段
+var precipitation = dataset.select(['HQprecipitation']).first();
+
+// 设置降水可视化参数
+var precipitationVis = {
+  min: 0,
+  max: 12,
+  palette: ['blue', 'purple', 'cyan', 'green', 'yellow', 'red'],
+};
+var countries = ee.FeatureCollection('FAO/GAUL/2015/level0'); 
+var italy = countries.filter(ee.Filter.eq('ADM0_NAME', 'Thailand')); // 选择意大利
+
+var geometry = ee.Geometry.Rectangle([92, -10, 140, 23]); // 获取意大利的几何信息
+
+// 将地图中心设置为感兴趣区域
+Map.setCenter(-79.98, 23.32, 4);
+
+// 在地图上添加降水图层
+Map.addLayer(precipitation, precipitationVis, 'Precipitation');
+var precipitationVisImg = precipitation.visualize(precipitationVis);
+Export.image.toDrive({
+  image: precipitationVisImg,
+  description: 'precipitation_map',
+  folder: 'GEE_exports',
+  region: geometry,
+  scale: 1000,
+  crs: 'EPSG:4326',
+  maxPixels: 1e13
+});
+
