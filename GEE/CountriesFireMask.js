56097c34e2f4d6134d3150e94645486ae2dc7f62
Lijiaqian1 <95671887+Lijiaqian1@users.noreply.github.com>
2024-03-28 00:40:08 +0800

    Update CountriesFireMask.js

--- GEE/CountriesFireMask.js
+++ GEE/CountriesFireMask.js
@@ -1,29 +1,29 @@
 var dataset = ee.ImageCollection('MODIS/061/MYD14A2')
-                  .filterDate('2017-08-29', '2017-08-30'); // 过滤五月一号的图像
-var fireMask = dataset.select('FireMask').first(); // 获取第一幅图像，即五月一号的火灾掩模图像
+                  .filterDate('2017-08-29', '2017-08-30'); 
+var fireMask = dataset.select('FireMask').first();
 
 var fireMaskVis = {
   min: 3.0,
   max: 8.0,
-  palette: ['black', 'grey'] // ['blue','green','red'] for color map
+  palette: ['blue','green','red'] 
 };
 var countries = ee.FeatureCollection('FAO/GAUL/2015/level0'); 
-var italy = countries.filter(ee.Filter.eq('ADM0_NAME', 'Italy')); // 选择意大利
+var italy = countries.filter(ee.Filter.eq('ADM0_NAME', 'Italy')); 
 
-var geometry = italy.geometry(); // 获取意大利的几何信息
+var geometry = italy.geometry(); 
 
-Map.setCenter(12.5674, 41.8719, 6); // 将地图中心设置为意大利的中心
+Map.setCenter(12.5674, 41.8719, 6); 
 Map.addLayer(
     fireMask, fireMaskVis,
-    'Fire Mask'); // 在地图上添加火灾掩模图层
+    'Fire Mask'); 
 
-var fireMaskVisImg = fireMask.visualize(fireMaskVis); // 应用可视化参数到火灾掩模图像上
+var fireMaskVisImg = fireMask.visualize(fireMaskVis); 
 Export.image.toDrive({
   image: fireMaskVisImg,
   description: 'fire_mask_map',
   folder: 'GEE_exports',
-  region: geometry, // 指定导出的区域
-  scale: 1000, // 指定导出的分辨率
-  crs: 'EPSG:4326', // 指定投影坐标系
-  maxPixels: 1e13 // 指定最大像素数量
+  region: geometry, 
+  scale: 1000, 
+  crs: 'EPSG:4326', 
+  maxPixels: 1e13 
 });
