162a8a7d4f60b30d1d44bd9fa5b0e38285e4606a
Lijiaqian1 <95671887+Lijiaqian1@users.noreply.github.com>
2024-05-22 19:02:53 +0800

    Update ndvi.js

--- GEE/ndvi.js
+++ GEE/ndvi.js
@@ -1,7 +1,7 @@
 var geometry = ee.Geometry.Rectangle([92, 9, 110, 23]);
 
 var mod09a1_0 = ee.ImageCollection("MODIS/061/MOD09A1");
-// 批量裁剪
+
 var mod09a1 = mod09a1_0.map(function(img){
   return img.clip(geometry); 
 });
@@ -10,29 +10,26 @@ var start = ee.Date('2022-2-1');
 var end = ee.Date('2022-2-9');
 var numDays = 28;
 
-//选择波段并给波段命名
+
 var modisBands = ['sur_refl_b03','sur_refl_b04','sur_refl_b01','sur_refl_b02','sur_refl_b06','sur_refl_b07'];
 var IsBands = ['blue','green','red','nir','swir1','swir2'];
 
-
-//用于提取QA位的辅助函数，主要目的是遍历筛选
 function getQABits(image, start, end, newName) {
-  //计算需要提取的比特
+
   var pattern = 0;
   for (var i = start; i<= end; i++){
     pattern += Math.pow(2, i);
   }
-  //返回一个提取的QA单波段图像，并给波段起个新名字
   return image.select([0], [newName])
   .bitwiseAnd(pattern)
   .rightShift(start);
 }
 
-//去云函数
+
 function maskQuality(image){
-  var QA = image.select('StateQA');  //选择QA波段
-  var internalQuality = getQABits(QA,8,13,'internal_quality_flag');   //获得internal_cloud_algorithm_flag位置
-  return image.updateMask(internalQuality.eq(0));  //返回一个去云图像
+  var QA = image.select('StateQA');  
+  var internalQuality = getQABits(QA,8,13,'internal_quality_flag');   
+  return image.updateMask(internalQuality.eq(0));  
 }
 var visParams = {bands:['red','green','blue'], min:0, max:3000, gamma:1.3};
   
@@ -44,11 +41,9 @@ var visParams = {bands:['red','green','blue'], min:0, max:3000, gamma:1.3};
 for (var i = 0; i < numDays; i++){
   var iniDate = start.advance(i, 'day');
   var endDate = end.advance(i, 'day');
-  //筛选日期、波段并批量进行去云，获得无云（cloud free）影像集合
   var mod09a1_cf = mod09a1.filterDate(iniDate, endDate)
                         .map(maskQuality)
                         .select(modisBands,IsBands)
-  //筛选日期和波段，获取有云影像集合  
   var Cloud = mod09a1.filterDate(iniDate, endDate)
                     .select(modisBands,IsBands)
   
@@ -65,7 +60,7 @@ for (var i = 0; i < numDays; i++){
   }
   var mod09a1_ndvi = mod09a1_re.map(NDVI);
   
-  //NDVI显示配置，NDVI值范围是-1到1
+
   var visParam_ndvi = {
    min: -0.2,
    max: 0.8,
